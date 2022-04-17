d6979869f680ad330795ccd2a974bc6eebbf367f
ibaspinar <ihsan@baspinar.eu>
2022-04-17 22:31:37 +0200

    progressbar angelegt und label ergänzt und benannt auf loading

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,17 +30,17 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(204, 123);
+            this.label1.Location = new System.Drawing.Point(361, 395);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.Size = new System.Drawing.Size(56, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "TestLabel";
+            this.label1.Text = "Loading";
             // 
             // progressBar1
             // 
@@ -48,20 +48,23 @@
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(776, 23);
             this.progressBar1.TabIndex = 1;
+            this.progressBar1.Value = 30;
             // 
-            // progressBar2
+            // label2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(347, 386);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(100, 23);
-            this.progressBar2.TabIndex = 2;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(423, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0%";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -75,7 +78,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
